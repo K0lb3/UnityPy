7ed2ca33647175f7777a8cd65a8411828af7993e
W0lf/K0lb3 <rkolbe96@gmail.com>
2019-08-04 01:15:35 +0200

    setup rework

--- setup.py
+++ setup.py
@@ -1,6 +1,37 @@
-#!/usr/bin/env python3
+import setuptools
 
-from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-
-setup()
+setuptools.setup(
+    name="UnityPy",
+	packages = setuptools.find_packages(),
+    version="1.1.2.4",
+    author="K0lb3",
+    description="A pythonic port of AssetStudio by Perfare",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/K0lb3/UnityPy",
+	download_url="https://github.com/K0lb3/UnityPy/tarball/master",
+	keywords = ['Unity', 'unitypack', 'UnityPy',"unpack"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Topic :: Multimedia :: Graphics",
+    ],
+	install_requires=[
+		"decrunch",
+		"fsb5",
+		"lz4",
+		"Pillow",
+		"brotli",
+		"colorama",
+		"termcolor"
+    ],
+)
