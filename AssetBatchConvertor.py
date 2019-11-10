34a868df924d114362c3949e1a80b0550b280408
K0lb3 <rkolbe96@gmail.com>
2019-11-10 22:55:43 +0100

    1.2.4.4

--- AssetBatchConvertor.py
+++ AssetBatchConvertor.py
@@ -1,15 +1,14 @@
 import os
-import time
-
-import json
 from UnityPy import AssetsManager
-from statistics import mode 
-TYPES = ['Sprite','Texture2D','TextAsset']
+from collections import Counter
+import zipfile
+
+TYPES = ['Sprite', 'Texture2D', 'TextAsset']
 
 ROOT = os.path.dirname(os.path.realpath(__file__))
 
 # source folder
-ASSETS=os.path.join(ROOT,'assetbundle')
+ASSETS = os.path.join(ROOT, 'assets')
 # destination folder
 DST = os.path.join(ROOT, 'extracted')
 # number of dirs to ignore
@@ -22,93 +21,89 @@ IGNOR_DIR_COUNT = 2
 
 os.makedirs(DST, exist_ok=True)
 
-debug = True
 
 def main():
-    for root, dirs, files in os.walk(ASSETS, topdown=False):
-        if '.git' in root:
-            continue
-        for f in files:
-            print(f)
-            # load file
-            src = os.path.realpath(os.path.join(root, f))
-            b = AssetsManager()
-            b.load_file(src)
+	for root, dirs, files in os.walk(ASSETS, topdown=False):
+		if '.git' in root:
+			continue
+		for f in files:
+			print(f)
+			extension = os.path.splitext(f)[1]
+			src = os.path.realpath(os.path.join(root, f))
 
-            # iterate over assets
-            for asset in b.assets.values():
-                # assets without container / internal path will be ignored for now
-                if not asset.container:
-                    continue
-                
-                # check which mode we will have to use
-                num_cont = sum(1 for obj in asset.container.values() if obj.type in TYPES)
-                num_objs = sum(1 for obj in asset.objects.values() if obj.type in TYPES)
-
-                # check if container contains all important assets, if yes, just ignore the container
-                if num_objs <= num_cont * 2:
-                    for asset_path, obj in asset.container.items():
-                        fp = os.path.join(DST, *asset_path.split('/')[IGNOR_DIR_COUNT:])
-                        export_obj(obj, fp)
-
-                # otherwise use the container to generate a path for the normal objects
-                else:
-                    local_path = []
-                    extracted = []
-
-                    for asset_path in asset.container.keys():
-                        local_path.append(
-                            os.path.splitext(asset_path)[0]
-                        )
-
-                    if len(local_path) > 1:
-                        if len(set(local_path)) == 1:
-                            local_path = local_path[0]
-                        else:
-                            local_path = mode(local_path)
-                    else:
-                        local_path = local_path[0]
-                    local_path = os.path.join(DST, *local_path.split('/')[IGNOR_DIR_COUNT:])
+			if extension == ".zip":
+				archive = zipfile.ZipFile(src, 'r')
+				for zf in archive.namelist():
+					extract_assets(archive.open(zf))
+			else:
+				extract_assets(src)
 
 
-                    for obj in asset.objects.values():
-                        if obj.path_id not in extracted:
-                            extracted.extend(export_obj(obj, local_path, append_name=True))
+def extract_assets(src):
+	# load source
+	am = AssetsManager(src)
+
+	# iterate over assets
+	for asset in am.assets.values():
+		# assets without container / internal path will be ignored for now
+		if not asset.container:
+			continue
+
+		# check which mode we will have to use
+		num_cont = sum(1 for obj in asset.container.values() if obj.type in TYPES)
+		num_objs = sum(1 for obj in asset.objects.values() if obj.type in TYPES)
+
+		# check if container contains all important assets, if yes, just ignore the container
+		if num_objs <= num_cont * 2:
+			for asset_path, obj in asset.container.items():
+				fp = os.path.join(DST, *asset_path.split('/')[IGNOR_DIR_COUNT:])
+				export_obj(obj, fp)
+
+		# otherwise use the container to generate a path for the normal objects
+		else:
+			extracted = []
+			# find the most common path
+			occurence_count = Counter(os.path.splitext(asset_path)[0] for asset_path in asset.container.keys())
+			local_path = os.path.join(DST, *occurence_count.most_common(1)[0][0].split('/')[IGNOR_DIR_COUNT:])
+
+			for obj in asset.objects.values():
+				if obj.path_id not in extracted:
+					extracted.extend(export_obj(obj, local_path, append_name=True))
 
 
-def export_obj(obj, fp : str, append_name : bool = False) -> list:
-    if obj.type not in TYPES:
-        return []
-    data = obj.read()
-    if append_name:
-        fp = os.path.join(fp, data.name)
-    
-    fp, extension = os.path.splitext(fp)
-    os.makedirs(os.path.dirname(fp), exist_ok=True)
+def export_obj(obj, fp: str, append_name: bool = False) -> list:
+	if obj.type not in TYPES:
+		return []
+	data = obj.read()
+	if append_name:
+		fp = os.path.join(fp, data.name)
 
-    if obj.type == 'TextAsset':
-        if not extension:
-            extension = '.txt'
-        with open(f"{fp}{extension}", 'wb') as f:
-            f.write(data.script)
+	fp, extension = os.path.splitext(fp)
+	os.makedirs(os.path.dirname(fp), exist_ok=True)
 
-    elif obj.type == "Sprite":
-        extension = ".png"
-        data.image.save(f"{fp}{extension}")
+	if obj.type == 'TextAsset':
+		if not extension:
+			extension = '.txt'
+		with open(f"{fp}{extension}", 'wb') as f:
+			f.write(data.script)
 
-        return [obj.path_id, data.m_RD.texture.path_id, getattr(data.m_RD.alphaTexture,'path_id', None)]
+	elif obj.type == "Sprite":
+		extension = ".png"
+		data.image.save(f"{fp}{extension}")
 
-    elif obj.type == "Texture2D":
-        extension = ".png"
-        fp = f"{fp}{extension}"
-        if not os.path.exists(fp):
-            try:
-                data.image.save(fp)
-            except EOFError:
-                pass
-    
-    return [obj.path_id]
+		return [obj.path_id, data.m_RD.texture.path_id, getattr(data.m_RD.alphaTexture, 'path_id', None)]
+
+	elif obj.type == "Texture2D":
+		extension = ".png"
+		fp = f"{fp}{extension}"
+		if not os.path.exists(fp):
+			try:
+				data.image.save(fp)
+			except EOFError:
+				pass
+
+	return [obj.path_id]
 
 
 if __name__ == '__main__':
-    main()
+	main()
