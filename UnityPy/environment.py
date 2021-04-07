797389f335d31020693b3d853a1288eb79ddd99e
K0lb3 <rudolf.kolbe@st.ovgu.de>
2021-04-07 20:51:23 +0200

    Env: exception for error during APK asset parsing

--- UnityPy/environment.py
+++ UnityPy/environment.py
@@ -12,11 +12,11 @@ from .streams import EndianBinaryReader
 class Environment:
     files: dict
     path: str
-    
+
     def __init__(self, *args):
         self.files = {}
         self.path = "."
-        
+
         if args:
             for arg in args:
                 if isinstance(arg, str):
@@ -31,44 +31,51 @@ class Environment:
                         self.load_folder(arg)
                 else:
                     self.path = None
-                    self.files[str(len(self.files))] = self.load_file(stream=arg)
-        
+                    self.files[str(len(self.files))
+                               ] = self.load_file(stream=arg)
+
         if len(self.files) == 1:
             self.file = list(self.files.values())[0]
-    
+
     def load_files(self, files: list):
         """Loads all files (list) into the AssetsManager and merges .split files for common usage."""
         # ImportHelper.merge_split_assets(path)
         # to_read_file = ImportHelper.processing_split_files(files)
         self.load(files)
-    
+
     def load_folder(self, path: str):
         """Loads all files in the given path and its subdirs into the AssetsManager."""
         ImportHelper.merge_split_assets(path, True)
         files = ImportHelper.list_all_files(path)
         to_read_file = ImportHelper.processing_split_files(files)
         self.load(to_read_file)
-    
+
     def load(self, files: list):
         """Loads all files into the AssetsManager."""
         for f in files:
-            self.files[f[len(self.path):].lstrip("/\\")] = self.load_file(open(f, "rb"), self)
-    
+            self.files[f[len(self.path):].lstrip(
+                "/\\")] = self.load_file(open(f, "rb"), self)
+
     def load_file(self, stream, parent=None):
         if not parent:
             parent = self
         typ, reader = ImportHelper.check_file_type(stream)
-        if typ == FileType.AssetsFile:
-            return files.SerializedFile(reader, parent)
-        elif typ == FileType.BundleFile:
-            return files.BundleFile(reader, parent)
-        elif typ == FileType.WebFile:
-            return files.WebFile(reader, parent)
-        elif typ == FileType.ZIP:
-            self.load_zip_file(stream)
-        elif typ == FileType.ResourceFile:
+        try:
+            if typ == FileType.AssetsFile:
+                return files.SerializedFile(reader, parent)
+            elif typ == FileType.BundleFile:
+                return files.BundleFile(reader, parent)
+            elif typ == FileType.WebFile:
+                return files.WebFile(reader, parent)
+            elif typ == FileType.ZIP:
+                self.load_zip_file(stream)
+            elif typ == FileType.ResourceFile:
+                return EndianBinaryReader(stream)
+        except:
+            # just to be sure
+            # cuz the SerializedFile detection isn't perfect
             return EndianBinaryReader(stream)
-    
+
     def load_zip_file(self, value):
         buffer = None
         if isinstance(value, str) and os.path.exists(value):
@@ -77,7 +84,7 @@ class Environment:
             buffer = ZipFile(io.BytesIO(value))
         elif isinstance(value, (io.BufferedReader, io.BufferedIOBase)):
             buffer = value
-        
+
         z = ZipFile(buffer)
         for path in z.namelist():
             stream = z.open(path)
@@ -90,7 +97,8 @@ class Environment:
                     cur.files[d] = files.File(self)
                 cur = cur.files[d]
             cur.files[name] = self.load_file(stream, cur)
-    
+        z.close()
+
     @property
     def objects(self):
         def search(item):
@@ -98,17 +106,17 @@ class Environment:
             if not isinstance(item, Environment) and getattr(item, "objects", None):
                 # serialized file
                 return [val for val in item.objects.values()]
-            
+
             elif getattr(item, "files", None):  # WebBundle and BundleFile
                 # bundle
                 for item in item.files.values():
                     ret.extend(search(item))
                 return ret
-            
+
             return ret
-        
+
         return search(self)
-    
+
     @property
     def container(self):
         return {
@@ -117,10 +125,10 @@ class Environment:
             if isinstance(f, File)
             for path, obj in f.container.items()
         }
-    
+
     @property
     def assets(self):
         return self.files
-    
-    def get(self, key, default = None):
+
+    def get(self, key, default=None):
         return getattr(self, key, default)
