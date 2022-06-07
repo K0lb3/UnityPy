906012ed6b845233e74d9e23fd209911c241a95d
Rudolf Kolbe <rudolf.kolbe@st.ovgu.de>
2022-06-07 15:35:02 +0200

    improve splitfile handling

--- UnityPy/environment.py
+++ UnityPy/environment.py
@@ -1,16 +1,16 @@
-from functools import reduce
+from typing import List, Callable, Dict, Union
 import io
 import os
 from zipfile import ZipFile
 import re
 from . import files
-from .files import File
+from .files import File, ObjectReader
 from .enums import FileType
 from .helpers import ImportHelper
 from .streams import EndianBinaryReader
 from .files import SerializedFile
 
-reSplit = re.compile(r"(.+[/\\](\w+?\.split))\d+")
+reSplit = re.compile(r"(.*?([^\/\\]+?))\.split\d+")
 
 
 class Environment:
@@ -32,13 +32,16 @@ class Environment:
                             self.load_zip_file(arg)
                         else:
                             self.path = os.path.dirname(arg)
-                            self.load_files([arg])
+                            if reSplit.match(arg):
+                                self.load_files([arg])
+                            else:
+                                self.load_file(arg)
                     elif os.path.isdir(arg):
                         self.path = arg
                         self.load_folder(arg)
                 else:
                     self.path = None
-                    self.files[str(len(self.files))] = self.load_file(stream=arg)
+                    self.load_file(file=arg)
 
         if len(self.files) == 1:
             self.file = list(self.files.values())[0]
@@ -46,39 +49,12 @@ class Environment:
         if self.path == "":
             self.path = os.getcwd()
 
-    def load_files(self, files: list):
-        """Loads all files (list) into the AssetsManager and merges .split files for common usage."""
-        # filter split files
-        i = 0
-        split_files = []
-        for path in files:
-            splitMatch = reSplit.match(path)
-            if splitMatch:
-                data = []
-                basepath, basename = splitMatch.groups()
-
-                if basepath in split_files:
-                    continue
-
-                split_files.append(basepath)
-                data = []
-                for i in range(0, 999):
-                    item = f"{basepath}.split{i}"
-                    if item in files:
-                        with open(item, "rb") as f:
-                            data.append(f.read())
-                    elif i:
-                        break
-                data = b"".join(data)
-
-                self.files[basepath] = self.load_file(data, self, basename)
-            else:
-                self.files[path] = self.cabs[os.path.basename(path)] = self.load_file(
-                    open(path, "rb").read(), self, path
-                )
+    def load_files(self, files: List[str]):
+        """Loads all files (list) into the Environment and merges .split files for common usage."""
+        self.load_assets(files, lambda x: open(x, "rb"))
 
     def load_folder(self, path: str):
-        """Loads all files in the given path and its subdirs into the AssetsManager."""
+        """Loads all files in the given path and its subdirs into the Environment."""
         self.load_files(
             [
                 os.path.join(root, f)
@@ -87,52 +63,75 @@ class Environment:
             ]
         )
 
-    def reduce_path(self, fp: str) -> str:
-        return fp[len(self.path) :].lstrip("/\\")
-
     def load(self, files: list):
-        """Loads all files into the AssetsManager."""
+        """Loads all files into the Environment."""
         self.files.update(
             {
-                self.reduce_path(f): self.load_file(open(f, "rb"), self, f)
+                os.path.basename(f): self.load_file(open(f, "rb"), self, f)
                 for f in files
                 if os.path.exists(f)
             }
         )
 
-    def load_file(self, stream, parent=None, name: str = None):
+    def load_file(
+        self,
+        file: Union[io.IOBase, str],
+        parent: Union["Environment", File] = None,
+        name: str = None,
+    ):
         if not parent:
             parent = self
-        typ, reader = ImportHelper.check_file_type(stream)
+        if isinstance(file, str):
+            split_match = reSplit.match(file)
+            if split_match:
+                basepath, basename = split_match.groups()
+                file = []
+                for i in range(0, 999):
+                    item = f"{basepath}.split{i}"
+                    if item in files:
+                        with open(item, "rb") as f:
+                            file.append(f.read())
+                    elif i:
+                        break
+                name = basepath
+                file = b"".join(file)
+            else:
+                name = file
+                file = open(file, "rb")
+
+        typ, reader = ImportHelper.check_file_type(file)
+
         try:
             stream_name = (
                 name
                 if name
                 else getattr(
-                    stream,
+                    file,
                     "name",
-                    str(stream.__hash__()) if hasattr(stream, "__hash__") else "",
+                    str(file.__hash__()) if hasattr(file, "__hash__") else "",
                 )
             )
+
             if typ == FileType.AssetsFile:
                 f = files.SerializedFile(reader, parent, name=stream_name)
                 self.register_cab(stream_name, f)
-                return f
             elif typ == FileType.BundleFile:
-                return files.BundleFile(reader, parent, name=stream_name)
+                f = files.BundleFile(reader, parent, name=stream_name)
             elif typ == FileType.WebFile:
-                return files.WebFile(reader, parent, name=stream_name)
+                f = files.WebFile(reader, parent, name=stream_name)
             elif typ == FileType.ZIP:
-                self.load_zip_file(stream)
+                f = self.load_zip_file(file)
             elif typ == FileType.ResourceFile:
-                f = EndianBinaryReader(stream)
+                f = EndianBinaryReader(file)
                 self.register_cab(stream_name, f)
-                return f
+
+            self.files[stream_name] = f
+            return f
         except Exception as e:
             # just to be sure
             # cuz the SerializedFile detection isn't perfect
             print("Error loading, reverting to EndianBinaryReader:\n", e)
-            return EndianBinaryReader(stream)
+            return EndianBinaryReader(file)
 
     def load_zip_file(self, value):
         buffer = None
@@ -144,34 +143,7 @@ class Environment:
             buffer = value
 
         z = ZipFile(buffer)
-        split_files = []
-        namelist = z.namelist()
-        for path in namelist:
-            splitMatch = reSplit.match(path)
-            if splitMatch:
-                data = []
-                basepath, basename = splitMatch.groups()
-
-                if basepath in split_files:
-                    continue
-
-                split_files.append(basepath)
-                data = []
-                for i in range(0, 999):
-                    item = f"{basepath}.split{i}"
-                    if item in namelist:
-                        with z.open(item) as f:
-                            data.append(f.read())
-                    elif i:
-                        break
-                data = b"".join(data)
-
-                self.files[basepath] = self.cabs[basename] = self.load_file(data, self)
-            else:
-                self.files[path] = self.cabs[os.path.basename(path)] = self.load_file(
-                    z.open(path).read(), self
-                )
-
+        self.load_assets(z.namelist(), lambda x: z.open(x, "r"))
         z.close()
 
     def save(self, pack="none"):
@@ -187,7 +159,9 @@ class Environment:
                     out.write(self.files[f].save(packer=pack))
 
     @property
-    def objects(self) -> list:
+    def objects(self) -> List[ObjectReader]:
+        """Returns a list of all objects in the Environment."""
+
         def search(item):
             ret = []
             if not isinstance(item, Environment) and getattr(item, "objects", None):
@@ -205,7 +179,8 @@ class Environment:
         return search(self)
 
     @property
-    def container(self):
+    def container(self) -> Dict[str, ObjectReader]:
+        """Returns a dictionary of all objects in the Environment."""
         return {
             path: obj
             for f in self.files.values()
@@ -232,8 +207,67 @@ class Environment:
     def get(self, key: str, default=None):
         return getattr(self, key, default)
 
-    def register_cab(self, name: str, item: object) -> None:
+    def register_cab(self, name: str, item: File) -> None:
+        """
+        Registers a cab file.
+
+        Parameters
+        ----------
+        name : str
+            The name of the cab file.
+        item : File
+            The file to register.
+        """
         self.cabs[os.path.basename(name.lower())] = item
 
-    def get_cab(self, name: str) -> object:
+    def get_cab(self, name: str) -> File:
+        """
+        Returns the cab file with the given name.
+
+        Parameters
+        ----------
+        name : str
+            The name of the cab file.
+
+        Returns
+        -------
+        File
+            The cab file.
+        """
         return self.cabs.get(os.path.basename(name.lower()), None)
+
+    def load_assets(self, assets: List[str], open_f: Callable[[str], io.IOBase]):
+        """
+        Load all assets from a list of files via the given open_f function.
+
+        Parameters
+        ----------
+        assets : List[str]
+            List of files to load.
+        open_f : Callable[[str], io.IOBase]
+            Function to open the files.
+            The function takes a file path and returns an io.IOBase object.
+        """
+        split_files = []
+        for path in assets:
+            splitMatch = reSplit.match(path)
+            if splitMatch:
+                basepath, basename = splitMatch.groups()
+
+                if basepath in split_files:
+                    continue
+
+                split_files.append(basepath)
+                data = []
+                for i in range(0, 999):
+                    item = f"{basepath}.split{i}"
+                    if item in assets:
+                        with open_f(item) as f:
+                            data.append(f.read())
+                    elif i:
+                        break
+                data = b"".join(data)
+                path = basepath
+            else:
+                data = open_f(path).read()
+            self.load_file(data, name=path)
