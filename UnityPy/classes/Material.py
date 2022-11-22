7a66e6f012504241d8fd3359ae85149a91c01b20
Rudolf Kolbe <rkolbe96@gmail.com>
2022-11-22 23:57:03 +0100

    Material cleanup

--- UnityPy/classes/Material.py
+++ UnityPy/classes/Material.py
@@ -9,24 +9,16 @@ class Material(NamedObject):
         self.m_Shader = PPtr(reader)  # Shader
 
         if version >= (2021,3): # 2021.3 and up
-            validKeywordSize = reader.read_int()
-            self.m_ValidKeywords = {}
-            for i in range(validKeywordSize):
-                self.m_ValidKeywords[i] = reader.read_aligned_string()
-
-            invalidKeywordSize = reader.read_int()
-            self.m_InvalidKeywords = {}
-            for i in range(invalidKeywordSize):
-                self.m_InvalidKeywords[i] = reader.read_aligned_string()
-
-            self.m_LightmapFlags = reader.read_u_int()
-            
+            self.m_ValidKeywords = .read_string_array()
+            self.m_InvalidKeywords = .read_string_array()
         elif version >= (5,):  # 5.0 and up
             self.m_ShaderKeywords = reader.read_aligned_string()
             self.m_LightmapFlags = reader.read_u_int()
-
         elif version >= (4, 1):  # 4.x
             self.m_ShaderKeywords = reader.read_string_array()
+
+        if version >= (5,):
+            self.m_LightmapFlags = reader.read_u_int()
 
         if version >= (5, 6):  # 5.6 and up
             self.m_EnableInstancingVariants = reader.read_boolean()
@@ -37,12 +29,10 @@ class Material(NamedObject):
             self.m_CustomRenderQueue = reader.read_int()
 
         if version >= (5, 1):  # 5.1 and up
-            stringTagMapSize = reader.read_int()
-            self.stringTagMap = {}
-            for _ in range(stringTagMapSize):
-                first = reader.read_aligned_string()
-                second = reader.read_aligned_string()
-                self.stringTagMap[first] = second
+            self.stringTagMap = {
+                reader.read_aligned_string(): reader.read_aligned_string()
+                for _ in range(reader.read_int())
+            }
 
         if version >= (5, 6):  # 5.6 and up
             self.disabledShaderPasses = reader.read_string_array()
@@ -59,27 +49,23 @@ class UnityTexEnv:
 
 class UnityPropertySheet:
     def __init__(self, reader):
-        m_TexEnvsSize = reader.read_int()
-        self.m_TexEnvs = {}
-        for i in range(m_TexEnvsSize):
-            key = reader.read_aligned_string()
-            self.m_TexEnvs[key] = UnityTexEnv(reader)
+        self.m_TexEnvs = {
+            reader.read_aligned_string(): UnityTexEnv(reader)
+            for _ in range(reader.read_int())
+        }
 
         if reader.version >= (2021,): # 2021.1 and up
-            m_IntsSize = reader.read_int()
-            self.m_Ints = {}
-            for i in range(m_IntsSize):
-                key = reader.read_aligned_string()
-                self.m_Ints[key] = reader.read_int()
+            self.m_Ints = {
+                reader.read_aligned_string(): reader.read_int()
+                for _ in range(reader.read_int())
+            }
 
-        m_FloatsSize = reader.read_int()
-        self.m_Floats = {}
-        for i in range(m_FloatsSize):
-            key = reader.read_aligned_string()
-            self.m_Floats[key] = reader.read_float()
+        self.m_Floats = {
+            reader.read_aligned_string(): reader.read_float()
+            for _ in range(reader.read_int())
+        }
 
-        m_ColorsSize = reader.read_int()
-        self.m_Colors = {}
-        for i in range(m_ColorsSize):
-            key = reader.read_aligned_string()
-            self.m_Colors[key] = reader.read_color4()
+        self.m_Colors = {
+            reader.read_aligned_string(): reader.read_color4()
+            for _ in range(reader.read_int())
+        }
