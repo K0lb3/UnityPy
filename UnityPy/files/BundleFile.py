K0lb3/UnityPy

commit 9232cdd57e93f140b2d7431905baff40e05488bc

Support Encryption of CN Builds (#157)
* implement CN Encryption for BundlePackages

* Improve Decrypion and BundleFile handling

* Update README.md

* Update README.md

UnityPy/files/BundleFile.py
@@ -1,11 +1,19 @@
-from . import File
-from ..helpers import CompressionHelper
-from ..streams import EndianBinaryReader, EndianBinaryWriter
-import re
+# TODO: implement encryption for saving files
 from collections import namedtuple
+import re
+from typing import Tuple
+
+from . import File
+from ..enums import ArchiveFlags, ArchiveFlagsOld, CompressionFlags
+from ..helpers import ArchiveStorageManager, CompressionHelper
+from ..streams import EndianBinaryReader, EndianBinaryWriter
+
+from .. import config
+
 
 BlockInfo = namedtuple("BlockInfo", "uncompressedSize compressedSize flags")
 DirectoryInfoFS = namedtuple("DirectoryInfoFS", "offset size flags path")
+reVersion = re.compile(r"(\d+)\.(\d+)\.(\d+)\w.+")
 
 
 class BundleFile(File.File):
@@ -14,6 +22,8 @@ class BundleFile(File.File):
     signature: str
     version_engine: str
     version_player: str
+    dataflags: Tuple[ArchiveFlags, ArchiveFlagsOld]
+    decryptor: ArchiveStorageManager.ArchiveStorageDecryptor = None
 
     def __init__(self, reader: EndianBinaryReader, parent: File, name: str = None):
         super().__init__(parent=parent, name=name)
@@ -23,13 +33,13 @@ class BundleFile(File.File):
         self.version_engine = reader.read_string_to_null()
 
         if signature == "UnityArchive":
-            raise NotImplemented("BundleFile - UnityArchive")
+            raise NotImplementedError("BundleFile - UnityArchive")
         elif signature in ["UnityWeb", "UnityRaw"]:
             m_DirectoryInfo, blocksReader = self.read_web_raw(reader)
         elif signature == "UnityFS":
             m_DirectoryInfo, blocksReader = self.read_fs(reader)
         else:
-            raise NotImplemented(f"Unknown Bundle signature: {signature}")
+            raise NotImplementedError(f"Unknown Bundle signature: {signature}")
 
         self.read_files(blocksReader, m_DirectoryInfo)
 
@@ -76,25 +86,44 @@ class BundleFile(File.File):
 
     def read_fs(self, reader: EndianBinaryReader):
         size = reader.read_long()
-        
+
         # header
         compressedSize = reader.read_u_int()
         uncompressedSize = reader.read_u_int()
-        self._data_flags = reader.read_u_int()
+        self.dataflags = reader.read_u_int()
+
+        version = tuple(map(int, reVersion.match(self.version_engine).groups()))
+        # https://issuetracker.unity3d.com/issues/files-within-assetbundles-do-not-start-on-aligned-boundaries-breaking-patching-on-nintendo-switch
+        # Unity CN introduced encryption before the alignment fix was introduced.
+        # Unity CN used the same flag for the encryption as later on the alignment fix,
+        # so we have to check the version to determine the correct flag set.
+        if (
+            version < (2020,)
+            or (version[0] == 2020 and version < (2020, 3, 34))
+            or (version[0] == 2021 and version < (2021, 3, 2))
+            or (version[0] == 2022 and version < (2022, 1, 1))
+        ):
+            self.dataflags = ArchiveFlagsOld(self.dataflags)
+        else:
+            self.dataflags = ArchiveFlags(self.dataflags)
 
         if self.version >= 7:
             reader.align_stream(16)
 
         start = reader.Position
-        if self._data_flags & 0x80 != 0:  # kArchiveBlocksInfoAtTheEnd
+        if (
+            self.dataflags & ArchiveFlags.BlocksInfoAtTheEnd
+        ):  # kArchiveBlocksInfoAtTheEnd
             reader.Position = reader.Length - compressedSize
             blocksInfoBytes = reader.read_bytes(compressedSize)
             reader.Position = start
         else:  # 0x40 kArchiveBlocksAndDirectoryInfoCombined
+            if self.dataflags & self.dataflags.UsesAssetBundleEncryption:
+                self.decryptor = ArchiveStorageManager.ArchiveStorageDecryptor(reader)
             blocksInfoBytes = reader.read_bytes(compressedSize)
 
-        blocksInfoBytes = decompress_data(
-            blocksInfoBytes, uncompressedSize, self._data_flags
+        blocksInfoBytes = self.decompress_data(
+            blocksInfoBytes, uncompressedSize, self.dataflags
         )
         blocksInfoReader = EndianBinaryReader(blocksInfoBytes, offset=start)
 
@@ -124,17 +153,21 @@ class BundleFile(File.File):
         if m_BlocksInfo:
             self._block_info_flags = m_BlocksInfo[0].flags
 
-        if self._data_flags & 0x200:
+        if (
+            isinstance(self.dataflags, ArchiveFlags)
+            and self.dataflags & ArchiveFlags.BlockInfoNeedPaddingAtStart
+        ):
             reader.align_stream(16)
 
         blocksReader = EndianBinaryReader(
             b"".join(
-                decompress_data(
+                self.decompress_data(
                     reader.read_bytes(blockInfo.compressedSize),
                     blockInfo.uncompressedSize,
                     blockInfo.flags,
+                    i,
                 )
-                for blockInfo in m_BlocksInfo
+                for i, blockInfo in enumerate(m_BlocksInfo)
             ),
             offset=(blocksInfoReader.real_offset()),
         )
@@ -166,9 +199,11 @@ class BundleFile(File.File):
         writer.write_string_to_null(self.version_engine)
 
         if self.signature == "UnityArchive":
-            raise NotImplemented("BundleFile - UnityArchive")
+            raise NotImplementedError("BundleFile - UnityArchive")
         elif self.signature in ["UnityWeb", "UnityRaw"]:
-            raise NotImplemented("Saving Unity Web and Raw bundles isn't supported yet")
+            raise NotImplementedError(
+                "Saving Unity Web and Raw bundles isn't supported yet"
+            )
             # self.save_web_raw(writer)
         elif self.signature == "UnityFS":
             if not packer or packer == "none":
@@ -184,7 +219,7 @@ class BundleFile(File.File):
             elif isinstance(packer, tuple):
                 self.save_fs(writer, *packer)
             else:
-                raise NotImplemented("UnityFS - Packer:", packer)
+                raise NotImplementedError("UnityFS - Packer:", packer)
         return writer.bytes
 
     def save_fs(self, writer: EndianBinaryWriter, data_flag: int, block_info_flag: int):
@@ -281,7 +316,9 @@ class BundleFile(File.File):
 
         # file block info
         if not data_flag & 0x40:
-            raise NotImplementedError("UnityPy always writes DirectoryInfo, so data_flag must include 0x40")
+            raise NotImplementedError(
+                "UnityPy always writes DirectoryInfo, so data_flag must include 0x40"
+            )
         # file count
         block_writer.write_int(len(files))
         offset = 0
@@ -341,38 +378,39 @@ class BundleFile(File.File):
             if data_flag & 0x200:
                 writer.align_stream(16)
             writer.write(file_data)
-        
+
         writer_end_pos = writer.Position
         writer.Position = writer_header_pos
         # correct file size
         writer.write_long(writer_end_pos)
         writer.Position = writer_end_pos
 
+    def decompress_data(
+        self, compressed_data: bytes, uncompressed_size: int, flags: int, index: int = 0
+    ) -> bytes:
+        """
+        Parameters
+        ----------
+        compressed_data : bytes
+            The compressed data.
+        uncompressed_size : int
+            The uncompressed size of the data.
+        flags : int
+            The flags of the data.
 
-def decompress_data(
-    compressed_data: bytes, uncompressed_size: int, flags: int
-) -> bytes:
-    """
-    Parameters
-    ----------
-    compressed_data : bytes
-        The compressed data.
-    uncompressed_size : int
-        The uncompressed size of the data.
-    flags : int
-        The flags of the data.
+        Returns
+        -------
+        bytes
+            The decompressed data."""
+        comp_flag = flags & ArchiveFlags.CompressionTypeMask
 
-    Returns
-    -------
-    bytes
-        The decompressed data."""
-    switch = flags & 0x3F
-
-    if switch == 1:  # LZMA
-        return CompressionHelper.decompress_lzma(compressed_data)
-    elif switch in [2, 3]:  # LZ4, LZ4HC
-        return CompressionHelper.decompress_lz4(compressed_data, uncompressed_size)
-    elif switch == 4:  # LZHAM
-        raise NotImplementedError("LZHAM decompression not implemented")
-    else:
-        return compressed_data
+        if comp_flag == CompressionFlags.LZMA:  # LZMA
+            return CompressionHelper.decompress_lzma(compressed_data)
+        elif comp_flag in [CompressionFlags.LZ4, CompressionFlags.LZ4HC]:  # LZ4, LZ4HC
+            if flags & 0x100:
+                compressed_data = self.decryptor.decrypt_block(compressed_data, index)
+            return CompressionHelper.decompress_lz4(compressed_data, uncompressed_size)
+        elif comp_flag == CompressionFlags.LZHAM:  # LZHAM
+            raise NotImplementedError("LZHAM decompression not implemented")
+        else:
+            return compressed_data

UnityPy/enums/BundleFile.py
@@ -1,0 +1,26 @@
+from enum import IntFlag
+
+
+class CompressionFlags(IntFlag):
+    NONE = 0
+    LZMA = 1
+    LZ4 = 2
+    LZ4HC = 3
+    LZHAM = 4
+
+
+class ArchiveFlagsOld(IntFlag):
+    CompressionTypeMask = 0x3F
+    BlocksAndDirectoryInfoCombined = 0x40
+    BlocksInfoAtTheEnd = 0x80
+    OldWebPluginCompatibility = 0x100
+    UsesAssetBundleEncryption = 0x200
+
+
+class ArchiveFlags(IntFlag):
+    CompressionTypeMask = 0x3F
+    BlocksAndDirectoryInfoCombined = 0x40
+    BlocksInfoAtTheEnd = 0x80
+    OldWebPluginCompatibility = 0x100
+    BlockInfoNeedPaddingAtStart = 0x200
+    UsesAssetBundleEncryption = 0x400

UnityPy/__init__.py
@@ -1,6 +1,7 @@
 __version__ = "1.9.20"
 
 from .environment import Environment
+from .helpers.ArchiveStorageManager import set_assetbundle_decrypt_key
 
 
 def load(*args):

UnityPy/config.py
@@ -2,5 +2,7 @@
 FALLBACK_UNITY_VERSION = "2.5.0f5"
 # determines if the typetree structures for the Object types will be parsed
 # disabling this will reduce the load time by a lot (half of the time is spend on parsing the typetrees)
-#  but it will also prevent saving an edited file 
+#  but it will also prevent saving an edited file
 SERIALIZED_FILE_PARSE_TYPETREE = True
+# enable for chinese version with AssetBundle encryption
+USE_CN_ENCRYPTION = False

UnityPy/enums/__init__.py
@@ -1,5 +1,6 @@
 from .Audio import AudioType, AudioCompressionFormat, AUDIO_TYPE_EXTEMSION
 from .BuildTarget import BuildTarget
+from .BundleFile import ArchiveFlags, ArchiveFlagsOld, CompressionFlags
 from .ClassIDType import ClassIDType
 from .FileType import FileType
 from .TextureFormat import TextureFormat

UnityPy/files/SerializedFile.py
@@ -578,7 +578,7 @@ class SerializedFile(File.File):
 
             # calc children count
             children_count = 0
-            for node2 in nodes[i + 1 :]:
+            for node2 in nodes[i + 1:]:
                 if node2.m_Level == node.m_Level:
                     break
                 if node2.m_Level == node.m_Level - 1:

UnityPy/helpers/ArchiveStorageManager.py
@@ -1,0 +1,117 @@
+# based on: https://github.com/Razmoth/PGRStudio/blob/master/AssetStudio/PGR/PGR.cs
+from typing import Tuple, Union
+
+from ..streams import EndianBinaryReader
+
+UNITY3D_SIGNATURE = b"#$unity3dchina!@"
+DECRYPT_KEY: bytes = None
+
+
+def set_assetbundle_decrypt_key(key: Union[bytes, str]):
+    if isinstance(key, str):
+        key = key.encode("utf-8", "surrogateescape")
+    if len(key) != 16:
+        raise ValueError(
+            f"AssetBundle Key length is wrong. It should be 16 bytes and now is {len(key)} bytes."
+        )
+    global DECRYPT_KEY
+    DECRYPT_KEY = key
+
+
+def read_vector(reader: EndianBinaryReader) -> Tuple[bytes, bytes]:
+    data = reader.read_bytes(0x10)
+    key = reader.read_string_to_null().encode("utf-8", "surrogateescape")
+
+    return data, key
+
+
+def decrypt_key(key: bytes, data: bytes, keybytes: bytes):
+    from Crypto.Cipher import AES
+
+    key = AES.new(keybytes, AES.MODE_ECB).encrypt(key)
+    return bytes(x ^ y for x, y in zip(data, key))
+
+
+def to_uint4_array(source: bytes, offset: int = 0):
+    buffer = bytearray(len(source) * 2)
+    for j in range(len(source)):
+        buffer[j * 2] = source[offset + j] >> 4
+        buffer[j * 2 + 1] = source[offset + j] & 15
+    return buffer
+
+
+class ArchiveStorageDecryptor:
+    unknown_1: int
+    index: bytes
+    substitute: bytes = bytes(0x10)
+
+    def __init__(self, reader: EndianBinaryReader) -> None:
+        if DECRYPT_KEY is None:
+            raise LookupError(
+                "The BundleFile is encrypted, but no key was provided!\nYou can set the key via UnityPy.set_assetbundle_decrypt_key(key)"
+            )
+        self.unknown_1 = reader.read_u_int()
+
+        # read vector data/key vectors
+        self.data, self.key = read_vector(reader)
+        self.data_sig, self.key_sig = read_vector(reader)
+
+        signature = decrypt_key(self.key_sig, self.data_sig, DECRYPT_KEY)
+        if signature != UNITY3D_SIGNATURE:
+            raise Exception(f"Invalid signature {signature} != {UNITY3D_SIGNATURE}")
+
+        data = decrypt_key(self.key, self.data, DECRYPT_KEY)
+        data = to_uint4_array(data)
+        self.index = data[:0x10]
+        self.substitute = bytes(
+            data[0x10 + i * 4 + j] for j in range(4) for i in range(4)
+        )
+
+    def decrypt_block(self, data: bytes, index: int):
+        offset = 0
+        size = len(data)
+        data = bytearray(data)
+        view = memoryview(data)
+        while offset < len(data):
+            offset += self.decrypt(view[offset:], index, size - offset)
+            index += 1
+        return data
+
+    def decrypt_byte(self, view: bytearray, offset: int, index: int):
+        b = (
+            self.substitute[((index >> 2) & 3) + 4]
+            + self.substitute[index & 3]
+            + self.substitute[((index >> 4) & 3) + 8]
+            + self.substitute[(index % 256 >> 6) + 12]
+        )
+        view[offset] = (
+            (self.index[view[offset] & 0xF] - b) & 0xF
+            | 0x10 * (self.index[view[offset] >> 4] - b)
+        ) % 256
+        b = view[offset]
+        return b, offset + 1, index + 1
+
+    def decrypt(self, data: bytearray, index: int, remaining: int):
+        offset = 0
+
+        curByte, offset, index = self.decrypt_byte(data, offset, index)
+        byteHigh = curByte >> 4
+        byteLow = curByte & 0xF
+
+        if byteHigh == 0xF:
+            b = 0xFF
+            while b == 0xFF:
+                b, offset, index = self.decrypt_byte(data, offset, index)
+                byteHigh += b
+
+        offset += byteHigh
+
+        if offset < remaining:
+            _, offset, index = self.decrypt_byte(data, offset, index)
+            _, offset, index = self.decrypt_byte(data, offset, index)
+            if byteLow == 0xF:
+                b = 0xFF
+                while b == 0xFF:
+                    b, offset, index = self.decrypt_byte(data, offset, index)
+
+        return offset

UnityPy/helpers/__init__.py
@@ -1,1 +1,1 @@
-from . import CompressionHelper, ImportHelper, TypeTreeHelper
+from . import ArchiveStorageManager, CompressionHelper, ImportHelper, TypeTreeHelper

commit 01906209c9f4104ebd2b4ef3825311d76b20a742

BundleFile: fix exception on file.save("original")
Regression in commit 9232cdd57e93f140b2d7431905baff40e05488bc,
which renamed _data_flags to dataflags.

UnityPy/files/BundleFile.py
@@ -223,7 +223,7 @@ class BundleFile(File.File):
             elif packer == "original":
                 self.save_fs(
                     writer,
-                    data_flag=self._data_flags,
+                    data_flag=self.dataflags,
                     block_info_flag=self._block_info_flags,
                 )
             elif packer == "lz4":