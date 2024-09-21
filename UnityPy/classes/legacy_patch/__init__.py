b30da31177c24b9c807d04802b20878ecbc55bdc
Rudolf Kolbe <rkolbe96@gmail.com>
2024-09-21 21:28:04 +0200

    add legacy_patch/Texture2DArray

--- UnityPy/classes/legacy_patch/__init__.py
+++ UnityPy/classes/legacy_patch/__init__.py
@@ -2,5 +2,6 @@ from .AudioClip import AudioClip
 from .Mesh import Mesh
 from .Renderer import Renderer
 from .Shader import Shader
-from .Texture2D import Texture2D
 from .Sprite import Sprite
+from .Texture2D import Texture2D
+from .Texture2DArray import Texture2DArray
