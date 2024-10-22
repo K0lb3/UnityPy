f1ae85c4bf4b3747dcb7596a00310d93f7c590c5
Rudolf Kolbe <rkolbe96@gmail.com>
2024-10-22 21:36:52 +0200

    TypeTreeHelper.hpp - add real type comments

--- UnityPyBoost/TypeTreeHelper.hpp
+++ UnityPyBoost/TypeTreeHelper.hpp
@@ -32,22 +32,22 @@ typedef struct TypeTreeNodeObject
 {
     PyObject_HEAD
         // helper field - simple hash of type for faster comparison
-        unsigned int _data_type;
+        NodeDataType _data_type;
     bool _align;
-    PyObject *_clean_name;
+    PyObject *_clean_name; // str
     // used filds for fast access
-    PyObject *m_Children;
-    PyObject *m_Name;
+    PyObject *m_Children; // list of TypeTreeNodes
+    PyObject *m_Name;     // str
+    PyObject *m_Type;     // str
     // fields not used in C
-    PyObject *m_Level;
-    PyObject *m_Type;
-    PyObject *m_ByteSize;
-    PyObject *m_Version;
-    PyObject *m_TypeFlags;
-    PyObject *m_VariableCount;
-    PyObject *m_Index;
-    PyObject *m_MetaFlag;
-    PyObject *m_RefTypeHash;
+    PyObject *m_Level;         // legacy: /,   blob: u8
+    PyObject *m_ByteSize;      // legacy: i32, blob: i32
+    PyObject *m_Version;       // legacy: i32, blob: i16
+    PyObject *m_TypeFlags;     // legacy: i32, blob: u8
+    PyObject *m_VariableCount; // legacy: i32, blob: /
+    PyObject *m_Index;         // legacy: i32, blob: i32
+    PyObject *m_MetaFlag;      // legacy: i32, blob: i32
+    PyObject *m_RefTypeHash;   // legacy: /,   blob: u64
 } TypeTreeNodeObject;
 
 int add_typetreenode_to_module(PyObject *m);
