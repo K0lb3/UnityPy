d7590f5237ceeb0941be4a1e2dee301ddda15d7f
Rudolf Kolbe <rkolbe96@gmail.com>
2024-10-25 09:51:07 +0200

    1.21 - rewrite base file type from scratch

--- UnityPy/files/File.py
+++ UnityPy/files/File.py
@@ -1,165 +1,246 @@
 from __future__ import annotations
 
-from collections import namedtuple
-from os.path import basename
-from typing import TYPE_CHECKING, Dict, Optional
+from abc import ABC, ABCMeta, abstractmethod
+from collections import defaultdict
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Dict,
+    Iterator,
+    List,
+    Optional,
+    Self,
+    Type,
+    TypeVar,
+    cast,
+)
 
-from ..helpers import ImportHelper
+from attrs import define
+
 from ..streams import EndianBinaryReader, EndianBinaryWriter
 
 if TYPE_CHECKING:
-    from ..environment import Environment
+    from .ObjectInfo import ObjectInfo
 
-DirectoryInfo = namedtuple("DirectoryInfo", "path offset size")
+PARSEABLE_FILETYPES: List[Type[File]] = []
+
+T = TypeVar("T")
 
 
-class File(object):
+def parseable_filetype(cls: Type[T]) -> Type[T]:
+    """Decorator to register a class as parseable."""
+    assert issubclass(cls, File)
+    PARSEABLE_FILETYPES.append(cls)
+    return cls
+
+
+def parse_file(
+    reader: EndianBinaryReader,
+    name: str,
+    path: str,
+    parent: Optional[ContainerFile] = None,
+    is_dependency: bool = False,
+) -> Optional[File]:
+    start_pos = reader.tell()
+    for cls in PARSEABLE_FILETYPES:
+        try:
+            probe_result = cls.probe(reader)
+        except (EOFError, UnicodeDecodeError, IndexError):
+            probe_result = False
+
+        reader.seek(start_pos)
+
+        if probe_result:
+            file = cls(name, path, parent, is_dependency)
+            file.parse(reader)
+            return file
+
+
+class File(ABC, metaclass=ABCMeta):
     name: str
-    files: Dict[str, File]
-    environment: Environment
-    cab_file: str
-    is_changed: bool
-    signature: str
-    packer: str
+    path: str
+    parent: Optional[ContainerFile]
     is_dependency: bool
-    parent: Optional[File]
+    reader: Optional[EndianBinaryReader]
 
     def __init__(
         self,
-        parent: Optional[File] = None,
-        name: Optional[str] = None,
+        name: str,
+        path: str,
+        parent: Optional[ContainerFile] = None,
         is_dependency: bool = False,
+        reader: Optional[EndianBinaryReader] = None,
     ):
-        self.files = {}
-        self.is_changed = False
-        self.cab_file = "CAB-UnityPy_Mod.resS"
+        self.name = name
+        self.path = path
         self.parent = parent
-        self.environment = self.environment = (
-            getattr(parent, "environment", parent) if parent else None
-        )
-        self.name = basename(name) if isinstance(name, str) else ""
         self.is_dependency = is_dependency
+        self.reader = reader
 
-    def get_assets(self):
-        if isinstance(self, SerializedFile.SerializedFile):
-            return self
+    @classmethod
+    @abstractmethod
+    def probe(cls, reader: EndianBinaryReader) -> bool:
+        """Check if the file is of this type.
 
-        for f in self.files.values():
-            if isinstance(f, (BundleFile.BundleFile, WebFile.WebFile)):
-                for asset in f.get_assets():
-                    yield asset
-            elif isinstance(f, SerializedFile.SerializedFile):
-                yield f
+        Args:
+            reader (EndianBinaryReader): The reader to read from.
 
-    def get_filtered_objects(self, obj_types=[]):
-        if len(obj_types) == 0:
-            return self.get_objects()
-        for f in self.files.values():
-            if isinstance(f, (BundleFile.BundleFile, WebFile.WebFile)):
-                for obj in f.objects:
-                    if obj.type in obj_types:
-                        yield obj
-            elif isinstance(f, SerializedFile.SerializedFile):
-                for obj in f.objects.values():
-                    if obj.type in obj_types:
-                        yield obj
-
-    def get_objects(self):
-        for f in self.files.values():
-            if isinstance(f, (BundleFile.BundleFile, WebFile.WebFile)):
-                for obj in f.objects:
-                    yield obj
-            elif isinstance(f, SerializedFile.SerializedFile):
-                for obj in f.objects.values():
-                    yield obj
-            elif isinstance(f, ObjectReader.ObjectReader):
-                yield f
-
-    def read_files(self, reader: EndianBinaryReader, files: list):
-        # read file data and convert it
-        for node in files:
-            reader.Position = node.offset
-            name = node.path
-            node_reader = EndianBinaryReader(
-                reader.read(node.size), offset=(reader.BaseOffset + node.offset)
-            )
-            f = ImportHelper.parse_file(
-                node_reader, self, name, is_dependency=self.is_dependency
-            )
-
-            if isinstance(f, (EndianBinaryReader, SerializedFile.SerializedFile)):
-                if self.environment:
-                    self.environment.register_cab(name, f)
-
-            # required for BundleFiles
-            f.flags = getattr(node, "flags", 0)
-            self.files[name] = f
-
-    def get_writeable_cab(self, name: str = None):
+        Returns:
+            bool: Whether the file is of this type.
         """
-        Creates a new cab file in the bundle that contains the given data.
-        This is usefull for asset types that use resource files.
+        pass
+
+    @abstractmethod
+    def parse(self, reader: Optional[EndianBinaryReader] = None) -> Self:
+        """Parse the file from the reader.
+
+        Args:
+            reader (EndianBinaryReader): The reader to read from.
         """
+        pass
 
-        if not name:
-            name = self.cab_file
+    @abstractmethod
+    def dump(self, writer: Optional[EndianBinaryWriter] = None) -> EndianBinaryWriter:
+        """Dump the file to the writer.
 
-        if not name:
-            return None
+        Args:
+            writer (EndianBinaryWriter): The writer to write to.
+        """
+        pass
 
-        if name in self.files:
-            if isinstance(self.files[name], EndianBinaryWriter):
-                return self.files[name]
-            else:
-                raise ValueError(
-                    "This cab already exists and isn't an EndianBinaryWriter"
-                )
+    def __enter__(self):
+        return self
 
-        writer = EndianBinaryWriter()
-        # try to find another resource file to copy the flags from
-        for fname, f in self.files.items():
-            if fname.endswith(".resS"):
-                writer.flags = f.flags
-                writer.endian = f.endian
-                break
-        else:
-            writer.flags = 0
-        writer.name = name
-        self.files[name] = writer
+    def __exit__(self):
+        pass
+
+    @abstractmethod
+    def get_objects(self) -> List[ObjectInfo[Any]]:
+        """Get all objects contained in this file and its childs.
+
+        Returns:
+            List[Object]: The objects in this file.
+        """
+        pass
+
+    @abstractmethod
+    def get_containers(self) -> Dict[str, List[ObjectInfo[Any]]]:
+        """Get all containers contained in this file and its childs.
+
+        Returns:
+            List[Tuple[str, Object]]: The containers in this file.
+        """
+        pass
+
+    def _opt_get_set_reader(
+        self, reader: Optional[EndianBinaryReader] = None
+    ) -> EndianBinaryReader:
+        if reader is not None:
+            self.reader = reader
+        if self.reader is None:
+            raise ValueError("No reader provided")
+        return self.reader
+
+
+class ResourceFile(File):
+    reader: EndianBinaryReader  # type: ignore
+
+    @classmethod
+    def probe(cls, reader: EndianBinaryReader) -> bool:
+        return True
+
+    def parse(self, reader: Optional[EndianBinaryReader] = None) -> Self:
+        if reader is not None:
+            self.reader = reader  # type: ignore
+        return self
+
+    def dump(self, writer: Optional[EndianBinaryWriter] = None) -> EndianBinaryWriter:
+        if writer is None:
+            writer = EndianBinaryWriter(endian="<")
+        writer.write_bytes(self.reader.get_bytes())
         return writer
 
-    @property
-    def container(self):
-        return {
-            path: obj
-            for f in self.files.values()
-            if isinstance(f, File)
-            for path, obj in f.container.items()
-        }
+    def get_objects(self) -> List[ObjectInfo[Any]]:
+        raise ValueError("ResourceFile does not contain objects")
 
-    def get(self, key, default=None):
-        return getattr(self, key, default)
-
-    def keys(self):
-        return self.files.keys()
-
-    def items(self):
-        return self.files.items()
-
-    def values(self):
-        return self.files.values()
-
-    def __getitem__(self, item):
-        return self.files[item]
-
-    def __repr__(self):
-        return f"<{self.__class__.__name__}>"
-
-    def mark_changed(self):
-        if isinstance(self.parent, File):
-            self.parent.mark_changed()
-        self.is_changed = True
+    def get_containers(self) -> Dict[str, List[ObjectInfo[Any]]]:
+        raise ValueError("ResourceFile does not contain containers")
 
 
-# recursive import requires the import down here
-from . import BundleFile, ObjectReader, SerializedFile, WebFile
+@define(slots=True)
+class DirectoryInfo:
+    offset: int
+    size: int
+    path: str
+    flags: Optional[int] = None
+
+
+# TODO: refractor name to avoid confusion with get_container / container paths
+class ContainerFile(File, ABC, metaclass=ABCMeta):
+    childs: List[File]
+    directory_infos: List[DirectoryInfo]
+    directory_reader: Optional[EndianBinaryReader]
+
+    # @abstractmethod
+    # def get_serialized_files(self) -> List[SerializedFile]:
+    #     """Get all serialized files contained in this file and its childs.
+
+    #     Returns:
+    #         List[SerializedFile]: The serialized files in this file.
+    #     """
+    #     pass
+    def __init__(
+        self,
+        name: str,
+        path: str,
+        parent: Optional[ContainerFile] = None,
+        is_dependency: bool = False,
+        reader: Optional[EndianBinaryReader] = None,
+    ):
+        super().__init__(name, path, parent, is_dependency, reader)
+        self.childs = []
+        self.directory_infos = []
+        self.directory_reader = None
+
+    def parse_files(self):
+        """Parse the directory infos and all files in this file."""
+        if self.directory_reader is None:
+            raise ValueError("directory_reader is not set")
+
+        for info in self.directory_infos:
+            path_parts = info.path.split("/")
+            name = path_parts[-1]
+            path = f"{self.path}/{info.path}"
+
+            # subreader = self.directory_reader.create_sub_reader(info.offset, info.size)
+            # subreader.seek(0)
+            self.directory_reader.seek(info.offset)
+            file = parse_file(
+                self.directory_reader, name, path, self, self.is_dependency
+            )
+            self.childs.append(
+                file
+                if file is not None
+                else ResourceFile(name, path, self, False, self.directory_reader)
+            )
+
+    def traverse(self) -> Iterator[File]:
+        if not self.childs and self.directory_infos:
+            self.parse_files()
+        for child in self.childs:
+            if isinstance(child, ContainerFile):
+                yield from child.traverse()
+            elif isinstance(child, ResourceFile):
+                continue
+            yield child
+
+    def get_objects(self) -> List[ObjectInfo[Any]]:
+        return [obj for child in self.traverse() for obj in child.get_objects()]
+
+    def get_containers(self) -> Dict[str, List[ObjectInfo[Any]]]:
+        containers: Dict[str, List[ObjectInfo[Any]]] = defaultdict(list)
+        for child in self.traverse():
+            for container_path, obj in child.get_containers().items():
+                containers[container_path].extend(obj)
+
+        return cast(Dict[str, List[ObjectInfo[Any]]], containers)
