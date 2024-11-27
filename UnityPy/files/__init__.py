48b56d904016c33c1570a5229f340c1814c4c34a
Rudolf Kolbe <rkolbe96@gmail.com>
2024-11-27 14:44:33 +0100

    update UnityPy.files.__init__.py

--- UnityPy/files/__init__.py
+++ UnityPy/files/__init__.py
@@ -1,5 +1,16 @@
-from .File import File, DirectoryInfo
-from .SerializedFile import SerializedFile
-from .BundleFile import BundleFile
-from .WebFile import WebFile
-from .ObjectReader import ObjectReader
+from .BundleFile import (
+    BundleFile as BundleFile,
+)
+from .BundleFile import (
+    BundleFileFS as BundleFileFS,
+)
+from .BundleFile import (
+    BundleFileWeb as BundleFileWeb,
+)
+from .File import ContainerFile as ContainerFile
+from .File import File as File
+from .File import parse_file as parse_file
+from .ObjectReader import ObjectReader as ObjectReader
+from .SerializedFile import SerializedFile as SerializedFile
+from .WebFile import TuanjieWebFile as TuanjieWebFile
+from .WebFile import WebFile as WebFile
