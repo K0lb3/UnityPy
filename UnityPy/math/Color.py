5ed53214835757ce093287759865f1cda87bcddb
Harry Huang <harryhuang2652@qq.com>
2025-01-31 20:45:29 +0800

    refactor(math): Color

--- UnityPy/math/Color.py
+++ UnityPy/math/Color.py
@@ -8,16 +8,12 @@ class Color:
     A: float
 
     def __init__(self, r: float = 0.0, g: float = 0.0, b: float = 0.0, a: float = 0.0):
+        if not all(isinstance(v, (int, float)) for v in (r, g, b, a)):
+            raise TypeError("All components must be numeric.")
         self.R = r
         self.G = g
         self.B = b
         self.A = a
-
-    def __eq__(self, other):
-        if isinstance(other, Color):
-            return self.__dict__ == other.__dict__
-        else:
-            return False
 
     def __add__(self, other):
         return Color(
@@ -37,7 +33,7 @@ class Color:
         else:
             return Color(self.R * other, self.G * other, self.B * other, self.A * other)
 
-    def __div__(self, other):
+    def __truediv__(self, other):
         if isinstance(other, Color):
             return Color(
                 self.R / other.R, self.G / other.G, self.B / other.B, self.A / other.A
@@ -46,10 +42,13 @@ class Color:
             return Color(self.R / other, self.G / other, self.B / other, self.A / other)
 
     def __eq__(self, other):
-        return self.__dict__ == other.__dict__
+        if isinstance(other, Color):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
 
     def __ne__(self, other):
-        return self.__dict__ != other.__dict__
+        return not (self == other)
 
     def Vector4(self):
         return Vector4(self.R, self.G, self.B, self.A)
