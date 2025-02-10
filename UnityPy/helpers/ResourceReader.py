fd42f9c317382b22730ec080af1f7d71cfb76bdd
Harry Huang <harryhuang2652@qq.com>
2025-02-10 11:03:16 +0800

    refactor(helpers): get_resource_data in ResourceReader

--- UnityPy/helpers/ResourceReader.py
+++ UnityPy/helpers/ResourceReader.py
@@ -1,51 +1,37 @@
 import ntpath
 from ..streams import EndianBinaryReader
-from ..files import File
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from ..files.SerializedFile import SerializedFile
 
 
-def get_resource_data(*args):
-    """
-    Input:
-    Option 1:
-        0 - path - file path
-        1 - assets_file - SerializedFile
-        2 - offset -
-        3 - size -
-    Option 2:
-        0 - reader - EndianBinaryReader
-        1 - offset -
-        2 - size -
-
-    -> -2 = offset, -1 = size
-    """
-    if len(args) == 4:
-        res_path, assets_file, offset, size = args
-        basename = ntpath.basename(res_path)
-        name, ext = ntpath.splitext(basename)
-        possible_names = [
-            basename,
-            f"{name}.resource",
-            f"{name}.assets.resS",
-            f"{name}.resS",
-        ]
-        environment = assets_file.environment
-        reader = None
+def get_resource_data(res_path: str, assets_file: "SerializedFile", offset: int, size: int):
+    basename = ntpath.basename(res_path)
+    name, ext = ntpath.splitext(basename)
+    possible_names = [
+        basename,
+        f"{name}.resource",
+        f"{name}.assets.resS",
+        f"{name}.resS",
+    ]
+    environment = assets_file.environment
+    reader = None
+    for possible_name in possible_names:
+        reader = environment.get_cab(possible_name)
+        if reader:
+            break
+    if not reader:
+        assets_file.load_dependencies(possible_names)
         for possible_name in possible_names:
             reader = environment.get_cab(possible_name)
             if reader:
                 break
         if not reader:
-            assets_file.load_dependencies(possible_names)
-            for possible_name in possible_names:
-                reader = environment.get_cab(possible_name)
-                if reader:
-                    break
-            if not reader:
-                raise FileNotFoundError(f"Resource file {basename} not found")
-    elif len(args) == 3:
-        reader, offset, size = args
-    else:
-        raise TypeError(f"3 or 4 arguments required, but only {len(args)} given")
+            raise FileNotFoundError(f"Resource file {basename} not found")
+    return _get_resource_data(reader, offset, size)
 
+
+def _get_resource_data(reader: EndianBinaryReader, offset: int, size: int):
     reader.Position = offset
     return reader.read_bytes(size)
