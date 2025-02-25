7e7542bfeefcb4b4c1ec71445b8229a452539756
Harry Huang <harryhuang2652@qq.com>
2025-02-25 19:28:48 +0800

    feat(config): unset default fallback version

--- UnityPy/exceptions.py
+++ UnityPy/exceptions.py
@@ -1,5 +1,14 @@
 class TypeTreeError(Exception):
-    def __init__(self, message, nodes):            
-        # Call the base class constructor with the parameters it needs
+    def __init__(self, message, nodes):
         super().__init__(message)
         self.nodes = nodes
+
+
+class UnityVersionFallbackError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class UnityVersionFallbackWarning(UserWarning):
+    def __init__(self, message):
+        super().__init__(message)
