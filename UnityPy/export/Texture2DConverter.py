a6ef38cd615610497c395c182416254a21e8f045
Harry Huang <harryhuang2652@qq.com>
2025-06-06 20:39:36 +0800

    chore!(TextureSwizzler): rename `TEXTURE_FORMAT_BLOCK_SIZE_MAP`

--- UnityPy/export/Texture2DConverter.py
+++ UnityPy/export/Texture2DConverter.py
@@ -214,7 +214,7 @@ def image_to_texture2d(
 
         assert platform_blob is not None
         gobs_per_block = TextureSwizzler.get_switch_gobs_per_block(platform_blob)
-        block_size = TextureSwizzler.TEXTUREFORMAT_BLOCK_SIZE_MAP[s_tex_format]
+        block_size = TextureSwizzler.TEXTURE_FORMAT_BLOCK_SIZE_MAP[s_tex_format]
         width, height = TextureSwizzler.get_padded_texture_size(img.width, img.height, *block_size, gobs_per_block)
         switch_info = (block_size, gobs_per_block)
 
@@ -285,7 +285,7 @@ def parse_image_data(
 
         assert platform_blob is not None
         gobs_per_block = TextureSwizzler.get_switch_gobs_per_block(platform_blob)
-        block_size = TextureSwizzler.TEXTUREFORMAT_BLOCK_SIZE_MAP[texture_format]
+        block_size = TextureSwizzler.TEXTURE_FORMAT_BLOCK_SIZE_MAP[texture_format]
         width, height = TextureSwizzler.get_padded_texture_size(width, height, *block_size, gobs_per_block)
         image_data = TextureSwizzler.deswizzle(image_data, width, height, *block_size, gobs_per_block)
     else:
